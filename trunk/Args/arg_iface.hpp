9a47d9336e9fc6b257ae81444c9a6f210e9c2691
igor.mironchik@gmail.com <igor.mironchik@gmail.com@16ddb40b-240d-7851-124f-c17fcd2b4194>
2013-11-25 07:45:34 +0000

    Implemented ArgIface interface.

---
+++ trunk/Args/arg_iface.hpp
@@ -0,0 +1,85 @@
+
+/*!
+	\file
+
+	\author Igor Mironchik (igor.mironchik at gmail dot com).
+
+	Copyright (c) 2013 Igor Mironchik
+
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following
+	conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#ifndef ARGS__ARG_IFACE_HPP__INCLUDED
+#define ARGS__ARG_IFACE_HPP__INCLUDED
+
+// C++ include.
+#include <string>
+
+
+namespace Args {
+
+class Context;
+
+
+//
+// ArgIface
+//
+
+//! Interface for arguments.
+class ArgIface {
+public:
+	virtual ~ArgIface();
+
+	/*!
+		\return Argument for the given \arg name name.
+
+		\retval Pointer to the ArgIface if this argument handles
+			argument with the given \arg name name.
+		\retval nullptr if this argument doesn't know about
+			argument with name \arg name.
+	*/
+	virtual ArgIface * isItYou(
+		/*!
+			Name of the argument. Can be for example "-t" or
+			"--timeout".
+		*/
+		const std::string & name ) = 0;
+
+	/*!
+		Process argument's staff, for example take values from
+		context. This method invokes exactly at that moment when
+		parser has found this argument.
+	*/
+	virtual void process(
+		//! Context of the command line.
+		Context & context ) = 0;
+
+	//! \return Is this argument required?
+	virtual bool isRequired() const = 0;
+
+	//! \return Is this argument defined?
+	virtual bool isDefined() const = 0;
+}; // class ArgIface
+
+} /* namespace Args */
+
+#endif // ARGS__ARG_IFACE_HPP__INCLUDED
