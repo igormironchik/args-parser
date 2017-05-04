ada118afe1a23659000468739eb00d8d57a2793e
Igor Mironchik <igor.mironchik@gmail.com>
2017-05-04 13:09:43 +0300

    Changed examples.

--- samples/help/main.cpp
+++ samples/help/main.cpp
@@ -40,55 +40,55 @@ int wmain( int argc, wchar_t ** argv )
 int main( int argc, char ** argv )
 #endif
 {
-	/*
-		We create Args::CmdLine instance for parsing
-		command line arguments.
-	*/
-	Args::CmdLine cmd( argc, argv );
-
-	/*
-		And create our arguments.
-	*/
-
-	/*
-		This is argument with flag "-h" and name "--host".
-		He is with value and required.
-	*/
-	Args::Arg host( SL( 's' ), SL( "host" ),
-		// Argument is with value.
-		true,
-		// Argument is required.
-		true );
-	// Set description of the argument.
-	host.setDescription( SL( "Host. Can be \"localhost\", \"any\" or regular IP." ) );
-	// We can specify long description too.
-	host.setLongDescription( SL( "Host. This argument told to the application "
-		"where to open socket for communication." ) );
-
-	Args::Arg port( SL( 'p' ), SL( "port" ), true, true );
-	port.setDescription( SL( "Port number to create socket." ) );
-
-	/*
-		This argument have name "--timeout" only.
-		He is with value but optional.
-	*/
-	Args::Arg timeout( SL( "timeout" ), true );
-	//! This argument want to specify value specifier in the help. Let's do it.
-	timeout.setValueSpecifier( SL( "ms" ) );
-	timeout.setDescription( SL( "Timeout before new messages will be sent "
-		"in milliseconds." ) );
-
-	/*
-		We create help now.
-	*/
-	Args::Help help;
-	// Set executable name to the help printer.
-	help.setExecutable( argv[ 0 ] );
-	//! And set description of the application.
-	help.setAppDescription( SL( "This application just show "
-		"the power of Args." ) );
-
 	try {
+		/*
+			We create Args::CmdLine instance for parsing
+			command line arguments.
+		*/
+		Args::CmdLine cmd( argc, argv );
+
+		/*
+			And create our arguments.
+		*/
+
+		/*
+			This is argument with flag "-h" and name "--host".
+			He is with value and required.
+		*/
+		Args::Arg host( SL( 's' ), SL( "host" ),
+			// Argument is with value.
+			true,
+			// Argument is required.
+			true );
+		// Set description of the argument.
+		host.setDescription( SL( "Host. Can be \"localhost\", \"any\" or regular IP." ) );
+		// We can specify long description too.
+		host.setLongDescription( SL( "Host. This argument told to the application "
+			"where to open socket for communication." ) );
+
+		Args::Arg port( SL( 'p' ), SL( "port" ), true, true );
+		port.setDescription( SL( "Port number to create socket." ) );
+
+		/*
+			This argument have name "--timeout" only.
+			He is with value but optional.
+		*/
+		Args::Arg timeout( SL( "timeout" ), true );
+		//! This argument want to specify value specifier in the help. Let's do it.
+		timeout.setValueSpecifier( SL( "ms" ) );
+		timeout.setDescription( SL( "Timeout before new messages will be sent "
+			"in milliseconds." ) );
+
+		/*
+			We create help now.
+		*/
+		Args::Help help;
+		// Set executable name to the help printer.
+		help.setExecutable( argv[ 0 ] );
+		//! And set description of the application.
+		help.setAppDescription( SL( "This application just show "
+			"the power of Args." ) );
+
 		/*
 			Now add our argument to the command line.
 		*/
--- samples/qt_help/main.cpp
+++ samples/qt_help/main.cpp
@@ -38,56 +38,56 @@
 
 int main( int argc, char ** argv )
 {
-	/*
-		We create Args::CmdLine instance for parsing
-		command line arguments.
-	*/
-	Args::CmdLine cmd( argc, argv );
-
-	/*
-		And create our arguments.
-	*/
-
-	/*
-		This is argument with flag "-h" and name "--host".
-		He is with value and required.
-	*/
-	Args::Arg host( QChar( 's' ), QLatin1String( "host" ),
-		// Argument is with value.
-		true,
-		// Argument is required.
-		true );
-	// Set description of the argument.
-	host.setDescription( QLatin1String( "Host. Can be \"localhost\", "
-		"\"any\" or regular IP." ) );
-	// We can specify long description too.
-	host.setLongDescription( QLatin1String( "Host. This argument told to the "
-		"application where to open socket for communication." ) );
-
-	Args::Arg port( QChar( 'p' ), QLatin1String( "port" ), true, true );
-	port.setDescription( QLatin1String( "Port number to create socket." ) );
-
-	/*
-		This argument have name "--timeout" only.
-		He is with value but optional.
-	*/
-	Args::Arg timeout( QLatin1String( "timeout" ), true );
-	//! This argument want to specify value specifier in the help. Let's do it.
-	timeout.setValueSpecifier( QLatin1String( "ms" ) );
-	timeout.setDescription( QLatin1String( "Timeout before new messages will be "
-		"sent in milliseconds." ) );
-
-	/*
-		We create help now.
-	*/
-	Args::Help help;
-	// Set executable name to the help printer.
-	help.setExecutable( argv[ 0 ] );
-	//! And set description of the application.
-	help.setAppDescription( QLatin1String( "This application just show "
-		"the power of Args." ) );
-
 	try {
+		/*
+			We create Args::CmdLine instance for parsing
+			command line arguments.
+		*/
+		Args::CmdLine cmd( argc, argv );
+
+		/*
+			And create our arguments.
+		*/
+
+		/*
+			This is argument with flag "-h" and name "--host".
+			He is with value and required.
+		*/
+		Args::Arg host( QChar( 's' ), QLatin1String( "host" ),
+			// Argument is with value.
+			true,
+			// Argument is required.
+			true );
+		// Set description of the argument.
+		host.setDescription( QLatin1String( "Host. Can be \"localhost\", "
+			"\"any\" or regular IP." ) );
+		// We can specify long description too.
+		host.setLongDescription( QLatin1String( "Host. This argument told to the "
+			"application where to open socket for communication." ) );
+
+		Args::Arg port( QChar( 'p' ), QLatin1String( "port" ), true, true );
+		port.setDescription( QLatin1String( "Port number to create socket." ) );
+
+		/*
+			This argument have name "--timeout" only.
+			He is with value but optional.
+		*/
+		Args::Arg timeout( QLatin1String( "timeout" ), true );
+		//! This argument want to specify value specifier in the help. Let's do it.
+		timeout.setValueSpecifier( QLatin1String( "ms" ) );
+		timeout.setDescription( QLatin1String( "Timeout before new messages will be "
+			"sent in milliseconds." ) );
+
+		/*
+			We create help now.
+		*/
+		Args::Help help;
+		// Set executable name to the help printer.
+		help.setExecutable( argv[ 0 ] );
+		//! And set description of the application.
+		help.setAppDescription( QLatin1String( "This application just show "
+			"the power of Args." ) );
+
 		/*
 			Now add our argument to the command line.
 		*/
