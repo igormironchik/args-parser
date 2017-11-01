824f1101102794f609b7da9aa2e43ff41406a2bf
Igor Mironchik <igor.mironchik@gmail.com>
2017-11-01 16:28:50 +0300

    Testing.

--- tests/auto/api/main.cpp
+++ tests/auto/api/main.cpp
@@ -239,6 +239,419 @@ TEST( ArgAPI, TestAllIsOkWithEmptyCtor )
 	CHECK_CONDITION( !cmd.isDefined( SL( "-h" ) ) )
 }
 
+TEST( ArgAPI, TestGetterSetterOfArg )
+{
+	CmdLine cmd;
+
+	cmd.addArgWithFlagAndName( SL( '1' ), SL( "11" ), true, false,
+			SL( "desc" ), SL( "long desc" ), SL( "val" ), SL( "vs" ) )
+		.addArgWithFlagOnly( SL( '2' ), true, false,
+			SL( "desc" ), SL( "long desc" ), SL( "val" ), SL( "vs" ) )
+		.addArgWithNameOnly( SL( "33" ), true, false,
+			SL( "desc" ), SL( "long desc" ), SL( "val" ), SL( "vs" ) );
+
+	auto * a1 = static_cast< Arg* > ( cmd.findArgument( SL( "-1" ) ) );
+
+	CHECK_CONDITION( a1->description() == SL( "desc" ) )
+	CHECK_CONDITION( a1->longDescription() == SL( "long desc" ) )
+	CHECK_CONDITION( a1->value() == SL( "val" ) )
+	CHECK_CONDITION( a1->valueSpecifier() == SL( "vs" ) )
+	CHECK_CONDITION( a1->defaultValue() == SL( "val" ) )
+
+	const auto * a2 = static_cast< const Arg* > ( cmd.findArgument( SL( "-2" ) ) );
+
+	CHECK_CONDITION( a2->description() == SL( "desc" ) )
+	CHECK_CONDITION( a2->longDescription() == SL( "long desc" ) )
+	CHECK_CONDITION( a2->value() == SL( "val" ) )
+	CHECK_CONDITION( a2->valueSpecifier() == SL( "vs" ) )
+	CHECK_CONDITION( a2->defaultValue() == SL( "val" ) )
+
+	auto * a3 = static_cast< Arg* > ( cmd.findArgument( SL( "--33" ) ) );
+
+	CHECK_CONDITION( a3->description() == SL( "desc" ) )
+	CHECK_CONDITION( a3->longDescription() == SL( "long desc" ) )
+	CHECK_CONDITION( a3->value() == SL( "val" ) )
+	CHECK_CONDITION( a3->valueSpecifier() == SL( "vs" ) )
+	CHECK_CONDITION( a3->defaultValue() == SL( "val" ) )
+}
+
+TEST( ArgAPI, TestGetterSetterOfMultiArg )
+{
+	CmdLine cmd;
+
+	cmd.addMultiArg( SL( '1' ), SL( "11" ), true, false,
+			SL( "desc" ), SL( "long desc" ), SL( "val" ), SL( "vs" ) )
+		.addMultiArgWithFlagOnly( SL( '2' ), true, false,
+			SL( "desc" ), SL( "long desc" ), SL( "val" ), SL( "vs" ) )
+		.addMultiArgWithNameOnly( SL( "33" ), true, false,
+			SL( "desc" ), SL( "long desc" ), SL( "val" ), SL( "vs" ) );
+
+	auto * a1 = static_cast< Arg* > ( cmd.findArgument( SL( "-1" ) ) );
+
+	CHECK_CONDITION( a1->description() == SL( "desc" ) )
+	CHECK_CONDITION( a1->longDescription() == SL( "long desc" ) )
+	CHECK_CONDITION( a1->value() == SL( "val" ) )
+	CHECK_CONDITION( a1->valueSpecifier() == SL( "vs" ) )
+	CHECK_CONDITION( a1->defaultValue() == SL( "val" ) )
+
+	const auto * a2 = static_cast< const Arg* > ( cmd.findArgument( SL( "-2" ) ) );
+
+	CHECK_CONDITION( a2->description() == SL( "desc" ) )
+	CHECK_CONDITION( a2->longDescription() == SL( "long desc" ) )
+	CHECK_CONDITION( a2->value() == SL( "val" ) )
+	CHECK_CONDITION( a2->valueSpecifier() == SL( "vs" ) )
+	CHECK_CONDITION( a2->defaultValue() == SL( "val" ) )
+
+	auto * a3 = static_cast< Arg* > ( cmd.findArgument( SL( "--33" ) ) );
+
+	CHECK_CONDITION( a3->description() == SL( "desc" ) )
+	CHECK_CONDITION( a3->longDescription() == SL( "long desc" ) )
+	CHECK_CONDITION( a3->value() == SL( "val" ) )
+	CHECK_CONDITION( a3->valueSpecifier() == SL( "vs" ) )
+	CHECK_CONDITION( a3->defaultValue() == SL( "val" ) )
+}
+
+TEST( ArgAPI, TestGetterSetterOfMultiArgWithValues )
+{
+	CmdLine cmd;
+
+	cmd.addMultiArgWithDefaulValues( SL( '1' ), SL( "11" ), true, false,
+			SL( "desc" ), SL( "long desc" ), { SL( "val1" ), SL( "val2" ) },
+			SL( "vs" ) )
+		.addMultiArgWithFlagOnlyAndDefaultValues( SL( '2' ), true, false,
+			SL( "desc" ), SL( "long desc" ), { SL( "val1" ), SL( "val2" ) },
+			SL( "vs" ) )
+		.addMultiArgWithNameOnlyAndDefaultValues( SL( "33" ), true, false,
+			SL( "desc" ), SL( "long desc" ), { SL( "val1" ), SL( "val2" ) },
+			SL( "vs" ) );
+
+	auto * a1 = static_cast< MultiArg* > ( cmd.findArgument( SL( "-1" ) ) );
+
+	CHECK_CONDITION( a1->description() == SL( "desc" ) )
+	CHECK_CONDITION( a1->longDescription() == SL( "long desc" ) )
+	CHECK_CONDITION( a1->value() == SL( "val1" ) )
+	CHECK_CONDITION( a1->valueSpecifier() == SL( "vs" ) )
+	CHECK_CONDITION( a1->defaultValue() == SL( "val1" ) )
+	CHECK_CONDITION( a1->defaultValues().size() == 2 )
+	CHECK_CONDITION( a1->defaultValues().front() == SL( "val1" ) )
+	CHECK_CONDITION( a1->defaultValues().back() == SL( "val2" ) )
+
+	const auto * a2 = static_cast< const MultiArg* > ( cmd.findArgument( SL( "-2" ) ) );
+
+	CHECK_CONDITION( a2->description() == SL( "desc" ) )
+	CHECK_CONDITION( a2->longDescription() == SL( "long desc" ) )
+	CHECK_CONDITION( a2->value() == SL( "val1" ) )
+	CHECK_CONDITION( a2->valueSpecifier() == SL( "vs" ) )
+	CHECK_CONDITION( a2->defaultValue() == SL( "val1" ) )
+	CHECK_CONDITION( a2->defaultValues().size() == 2 )
+	CHECK_CONDITION( a2->defaultValues().front() == SL( "val1" ) )
+	CHECK_CONDITION( a2->defaultValues().back() == SL( "val2" ) )
+
+	auto * a3 = static_cast< MultiArg* > ( cmd.findArgument( SL( "--33" ) ) );
+
+	CHECK_CONDITION( a3->description() == SL( "desc" ) )
+	CHECK_CONDITION( a3->longDescription() == SL( "long desc" ) )
+	CHECK_CONDITION( a3->value() == SL( "val1" ) )
+	CHECK_CONDITION( a3->valueSpecifier() == SL( "vs" ) )
+	CHECK_CONDITION( a3->defaultValue() == SL( "val1" ) )
+	CHECK_CONDITION( a3->defaultValues().size() == 2 )
+	CHECK_CONDITION( a3->defaultValues().front() == SL( "val1" ) )
+	CHECK_CONDITION( a3->defaultValues().back() == SL( "val2" ) )
+}
+
+TEST( ArgAPI, TestGetterSetterOfArgAsCommand )
+{
+	CmdLine cmd;
+
+	cmd.addArgAsCommand( SL( "arg1" ), false, ValueOptions::OneValue,
+		SL( "desc" ), SL( "long desc" ), SL( "val" ), SL( "vs" ) )
+		.addArgAsCommandWithDefaulValues( SL( "arg2" ), false,
+			ValueOptions::ManyValues, SL( "desc" ), SL( "long desc" ),
+			{ SL( "val1" ), SL( "val2" ) }, SL( "vs" ) );
+
+	auto * a1 = static_cast< ArgAsCommand* > (
+		cmd.findArgument( SL( "arg1" ) ) );
+
+	CHECK_CONDITION( a1->description() == SL( "desc" ) )
+	CHECK_CONDITION( a1->longDescription() == SL( "long desc" ) )
+	CHECK_CONDITION( a1->value() == SL( "val" ) )
+	CHECK_CONDITION( a1->valueSpecifier() == SL( "vs" ) )
+	CHECK_CONDITION( a1->defaultValue() == SL( "val" ) )
+
+	const auto * a2 = static_cast< const ArgAsCommand* > (
+		cmd.findArgument( SL( "arg2" ) ) );
+
+	CHECK_CONDITION( a2->description() == SL( "desc" ) )
+	CHECK_CONDITION( a2->longDescription() == SL( "long desc" ) )
+	CHECK_CONDITION( a2->value() == SL( "val1" ) )
+	CHECK_CONDITION( a2->valueSpecifier() == SL( "vs" ) )
+	CHECK_CONDITION( a2->defaultValue() == SL( "val1" ) )
+	CHECK_CONDITION( a2->defaultValues().size() == 2 )
+	CHECK_CONDITION( a2->defaultValues().front() == SL( "val1" ) )
+	CHECK_CONDITION( a2->defaultValues().back() == SL( "val2" ) )
+}
+
+TEST( ArgAPI, TestGetterSetterOfSubCommand )
+{
+	CmdLine cmd;
+
+	cmd.addSubCommand( SL( "arg1" ), false, ValueOptions::OneValue,
+		SL( "desc" ), SL( "long desc" ), SL( "val" ), SL( "vs" ) )
+		.addSubCommandWithDefaultValues( SL( "arg2" ), false,
+			ValueOptions::ManyValues, SL( "desc" ), SL( "long desc" ),
+			{ SL( "val1" ), SL( "val2" ) }, SL( "vs" ) );
+
+	auto * a1 = static_cast< ArgAsCommand* > (
+		cmd.findArgument( SL( "arg1" ) ) );
+
+	CHECK_CONDITION( a1->description() == SL( "desc" ) )
+	CHECK_CONDITION( a1->longDescription() == SL( "long desc" ) )
+	CHECK_CONDITION( a1->value() == SL( "val" ) )
+	CHECK_CONDITION( a1->valueSpecifier() == SL( "vs" ) )
+	CHECK_CONDITION( a1->defaultValue() == SL( "val" ) )
+
+	const auto * a2 = static_cast< const ArgAsCommand* > (
+		cmd.findArgument( SL( "arg2" ) ) );
+
+	CHECK_CONDITION( a2->description() == SL( "desc" ) )
+	CHECK_CONDITION( a2->longDescription() == SL( "long desc" ) )
+	CHECK_CONDITION( a2->value() == SL( "val1" ) )
+	CHECK_CONDITION( a2->valueSpecifier() == SL( "vs" ) )
+	CHECK_CONDITION( a2->defaultValue() == SL( "val1" ) )
+	CHECK_CONDITION( a2->defaultValues().size() == 2 )
+	CHECK_CONDITION( a2->defaultValues().front() == SL( "val1" ) )
+	CHECK_CONDITION( a2->defaultValues().back() == SL( "val2" ) )
+}
+
+TEST( ArgAPI, TestGetterSetterOfArgInGroup )
+{
+	CmdLine cmd;
+
+	cmd.addAllOfGroup( SL( "group" ) )
+		.addArgWithFlagAndName( SL( '1' ), SL( "11" ), true, false,
+			SL( "desc" ), SL( "long desc" ), SL( "val" ), SL( "vs" ) )
+		.addArgWithFlagOnly( SL( '2' ), true, false,
+			SL( "desc" ), SL( "long desc" ), SL( "val" ), SL( "vs" ) )
+		.addArgWithNameOnly( SL( "33" ), true, false,
+			SL( "desc" ), SL( "long desc" ), SL( "val" ), SL( "vs" ) )
+		.end();
+
+	auto * a1 = static_cast< Arg* > ( cmd.findArgument( SL( "-1" ) ) );
+
+	CHECK_CONDITION( a1->description() == SL( "desc" ) )
+	CHECK_CONDITION( a1->longDescription() == SL( "long desc" ) )
+	CHECK_CONDITION( a1->value() == SL( "val" ) )
+	CHECK_CONDITION( a1->valueSpecifier() == SL( "vs" ) )
+	CHECK_CONDITION( a1->defaultValue() == SL( "val" ) )
+
+	const auto * a2 = static_cast< const Arg* > ( cmd.findArgument( SL( "-2" ) ) );
+
+	CHECK_CONDITION( a2->description() == SL( "desc" ) )
+	CHECK_CONDITION( a2->longDescription() == SL( "long desc" ) )
+	CHECK_CONDITION( a2->value() == SL( "val" ) )
+	CHECK_CONDITION( a2->valueSpecifier() == SL( "vs" ) )
+	CHECK_CONDITION( a2->defaultValue() == SL( "val" ) )
+
+	auto * a3 = static_cast< Arg* > ( cmd.findArgument( SL( "--33" ) ) );
+
+	CHECK_CONDITION( a3->description() == SL( "desc" ) )
+	CHECK_CONDITION( a3->longDescription() == SL( "long desc" ) )
+	CHECK_CONDITION( a3->value() == SL( "val" ) )
+	CHECK_CONDITION( a3->valueSpecifier() == SL( "vs" ) )
+	CHECK_CONDITION( a3->defaultValue() == SL( "val" ) )
+}
+
+TEST( ArgAPI, TestGetterSetterOfMultiArgInGroup )
+{
+	CmdLine cmd;
+
+	cmd.addAllOfGroup( SL( "group" ) )
+		.addMultiArg( SL( '1' ), SL( "11" ), true, false,
+			SL( "desc" ), SL( "long desc" ), SL( "val" ), SL( "vs" ) )
+		.addMultiArgWithFlagOnly( SL( '2' ), true, false,
+			SL( "desc" ), SL( "long desc" ), SL( "val" ), SL( "vs" ) )
+		.addMultiArgWithNameOnly( SL( "33" ), true, false,
+			SL( "desc" ), SL( "long desc" ), SL( "val" ), SL( "vs" ) )
+		.end();
+
+	auto * a1 = static_cast< Arg* > ( cmd.findArgument( SL( "-1" ) ) );
+
+	CHECK_CONDITION( a1->description() == SL( "desc" ) )
+	CHECK_CONDITION( a1->longDescription() == SL( "long desc" ) )
+	CHECK_CONDITION( a1->value() == SL( "val" ) )
+	CHECK_CONDITION( a1->valueSpecifier() == SL( "vs" ) )
+	CHECK_CONDITION( a1->defaultValue() == SL( "val" ) )
+
+	const auto * a2 = static_cast< const Arg* > ( cmd.findArgument( SL( "-2" ) ) );
+
+	CHECK_CONDITION( a2->description() == SL( "desc" ) )
+	CHECK_CONDITION( a2->longDescription() == SL( "long desc" ) )
+	CHECK_CONDITION( a2->value() == SL( "val" ) )
+	CHECK_CONDITION( a2->valueSpecifier() == SL( "vs" ) )
+	CHECK_CONDITION( a2->defaultValue() == SL( "val" ) )
+
+	auto * a3 = static_cast< Arg* > ( cmd.findArgument( SL( "--33" ) ) );
+
+	CHECK_CONDITION( a3->description() == SL( "desc" ) )
+	CHECK_CONDITION( a3->longDescription() == SL( "long desc" ) )
+	CHECK_CONDITION( a3->value() == SL( "val" ) )
+	CHECK_CONDITION( a3->valueSpecifier() == SL( "vs" ) )
+	CHECK_CONDITION( a3->defaultValue() == SL( "val" ) )
+}
+
+TEST( ArgAPI, TestGetterSetterOfMultiArgWithValuesInGroup )
+{
+	CmdLine cmd;
+
+	cmd.addAllOfGroup( SL( "group" ) )
+		.addMultiArgWithDefaulValues( SL( '1' ), SL( "11" ), true, false,
+			SL( "desc" ), SL( "long desc" ), { SL( "val1" ), SL( "val2" ) },
+			SL( "vs" ) )
+		.addMultiArgWithFlagOnlyAndDefaultValues( SL( '2' ), true, false,
+			SL( "desc" ), SL( "long desc" ), { SL( "val1" ), SL( "val2" ) },
+			SL( "vs" ) )
+		.addMultiArgWithNameOnlyAndDefaultValues( SL( "33" ), true, false,
+			SL( "desc" ), SL( "long desc" ), { SL( "val1" ), SL( "val2" ) },
+			SL( "vs" ) )
+		.end();
+
+	auto * a1 = static_cast< MultiArg* > ( cmd.findArgument( SL( "-1" ) ) );
+
+	CHECK_CONDITION( a1->description() == SL( "desc" ) )
+	CHECK_CONDITION( a1->longDescription() == SL( "long desc" ) )
+	CHECK_CONDITION( a1->value() == SL( "val1" ) )
+	CHECK_CONDITION( a1->valueSpecifier() == SL( "vs" ) )
+	CHECK_CONDITION( a1->defaultValue() == SL( "val1" ) )
+	CHECK_CONDITION( a1->defaultValues().size() == 2 )
+	CHECK_CONDITION( a1->defaultValues().front() == SL( "val1" ) )
+	CHECK_CONDITION( a1->defaultValues().back() == SL( "val2" ) )
+
+	const auto * a2 = static_cast< const MultiArg* > ( cmd.findArgument( SL( "-2" ) ) );
+
+	CHECK_CONDITION( a2->description() == SL( "desc" ) )
+	CHECK_CONDITION( a2->longDescription() == SL( "long desc" ) )
+	CHECK_CONDITION( a2->value() == SL( "val1" ) )
+	CHECK_CONDITION( a2->valueSpecifier() == SL( "vs" ) )
+	CHECK_CONDITION( a2->defaultValue() == SL( "val1" ) )
+	CHECK_CONDITION( a2->defaultValues().size() == 2 )
+	CHECK_CONDITION( a2->defaultValues().front() == SL( "val1" ) )
+	CHECK_CONDITION( a2->defaultValues().back() == SL( "val2" ) )
+
+	auto * a3 = static_cast< MultiArg* > ( cmd.findArgument( SL( "--33" ) ) );
+
+	CHECK_CONDITION( a3->description() == SL( "desc" ) )
+	CHECK_CONDITION( a3->longDescription() == SL( "long desc" ) )
+	CHECK_CONDITION( a3->value() == SL( "val1" ) )
+	CHECK_CONDITION( a3->valueSpecifier() == SL( "vs" ) )
+	CHECK_CONDITION( a3->defaultValue() == SL( "val1" ) )
+	CHECK_CONDITION( a3->defaultValues().size() == 2 )
+	CHECK_CONDITION( a3->defaultValues().front() == SL( "val1" ) )
+	CHECK_CONDITION( a3->defaultValues().back() == SL( "val2" ) )
+}
+
+TEST( ArgAPI, TestGetterSetterOfArgAsCommandInGroup )
+{
+	CmdLine cmd;
+
+	cmd.addAllOfGroup( SL( "group" ) )
+		.addArgAsCommand( SL( "arg1" ), false, ValueOptions::OneValue,
+		SL( "desc" ), SL( "long desc" ), SL( "val" ), SL( "vs" ) )
+		.addArgAsCommandWithDefaulValues( SL( "arg2" ), false,
+			ValueOptions::ManyValues, SL( "desc" ), SL( "long desc" ),
+			{ SL( "val1" ), SL( "val2" ) }, SL( "vs" ) )
+		.end();
+
+	auto * a1 = static_cast< ArgAsCommand* > (
+		cmd.findArgument( SL( "arg1" ) ) );
+
+	CHECK_CONDITION( a1->description() == SL( "desc" ) )
+	CHECK_CONDITION( a1->longDescription() == SL( "long desc" ) )
+	CHECK_CONDITION( a1->value() == SL( "val" ) )
+	CHECK_CONDITION( a1->valueSpecifier() == SL( "vs" ) )
+	CHECK_CONDITION( a1->defaultValue() == SL( "val" ) )
+
+	const auto * a2 = static_cast< const ArgAsCommand* > (
+		cmd.findArgument( SL( "arg2" ) ) );
+
+	CHECK_CONDITION( a2->description() == SL( "desc" ) )
+	CHECK_CONDITION( a2->longDescription() == SL( "long desc" ) )
+	CHECK_CONDITION( a2->value() == SL( "val1" ) )
+	CHECK_CONDITION( a2->valueSpecifier() == SL( "vs" ) )
+	CHECK_CONDITION( a2->defaultValue() == SL( "val1" ) )
+	CHECK_CONDITION( a2->defaultValues().size() == 2 )
+	CHECK_CONDITION( a2->defaultValues().front() == SL( "val1" ) )
+	CHECK_CONDITION( a2->defaultValues().back() == SL( "val2" ) )
+}
+
+TEST( ArgAPI, TestGetterSetterOfSubCommandInGroup )
+{
+	CmdLine cmd;
+
+	cmd.addAllOfGroup( SL( "group" ) )
+		.addSubCommand( SL( "arg1" ), false, ValueOptions::OneValue,
+		SL( "desc" ), SL( "long desc" ), SL( "val" ), SL( "vs" ) )
+		.addSubCommandWithDefaultValues( SL( "arg2" ), false,
+			ValueOptions::ManyValues, SL( "desc" ), SL( "long desc" ),
+			{ SL( "val1" ), SL( "val2" ) }, SL( "vs" ) )
+		.end();
+
+	auto * a1 = static_cast< ArgAsCommand* > (
+		cmd.findArgument( SL( "arg1" ) ) );
+
+	CHECK_CONDITION( a1->description() == SL( "desc" ) )
+	CHECK_CONDITION( a1->longDescription() == SL( "long desc" ) )
+	CHECK_CONDITION( a1->value() == SL( "val" ) )
+	CHECK_CONDITION( a1->valueSpecifier() == SL( "vs" ) )
+	CHECK_CONDITION( a1->defaultValue() == SL( "val" ) )
+
+	const auto * a2 = static_cast< const ArgAsCommand* > (
+		cmd.findArgument( SL( "arg2" ) ) );
+
+	CHECK_CONDITION( a2->description() == SL( "desc" ) )
+	CHECK_CONDITION( a2->longDescription() == SL( "long desc" ) )
+	CHECK_CONDITION( a2->value() == SL( "val1" ) )
+	CHECK_CONDITION( a2->valueSpecifier() == SL( "vs" ) )
+	CHECK_CONDITION( a2->defaultValue() == SL( "val1" ) )
+	CHECK_CONDITION( a2->defaultValues().size() == 2 )
+	CHECK_CONDITION( a2->defaultValues().front() == SL( "val1" ) )
+	CHECK_CONDITION( a2->defaultValues().back() == SL( "val2" ) )
+}
+
+TEST( ArgAPI, TestGetterSetterOfCommand )
+{
+	CmdLine cmd;
+
+	cmd.addCommand( SL( "cmd1" ), ValueOptions::OneValue,
+			SL( "desc" ), SL( "long desc" ), SL( "val" ),
+			SL( "vs" ) )
+		.end()
+		.addCommandWithDefaultValues( SL( "cmd2" ), ValueOptions::ManyValues,
+			SL( "desc" ), SL( "long desc" ), { SL( "val1" ), SL( "val2" ) },
+			SL( "vs" ) )
+		.end();
+
+	auto * a1 = static_cast< Command* > (
+		cmd.findArgument( SL( "cmd1" ) ) );
+
+	CHECK_CONDITION( a1->description() == SL( "desc" ) )
+	CHECK_CONDITION( a1->longDescription() == SL( "long desc" ) )
+	CHECK_CONDITION( a1->value() == SL( "val" ) )
+	CHECK_CONDITION( a1->valueSpecifier() == SL( "vs" ) )
+	CHECK_CONDITION( a1->defaultValue() == SL( "val" ) )
+
+	const auto * a2 = static_cast< const Command* > (
+		cmd.findArgument( SL( "cmd2" ) ) );
+
+	CHECK_CONDITION( a2->description() == SL( "desc" ) )
+	CHECK_CONDITION( a2->longDescription() == SL( "long desc" ) )
+	CHECK_CONDITION( a2->value() == SL( "val1" ) )
+	CHECK_CONDITION( a2->valueSpecifier() == SL( "vs" ) )
+	CHECK_CONDITION( a2->defaultValue() == SL( "val1" ) )
+	CHECK_CONDITION( a2->defaultValues().size() == 2 )
+	CHECK_CONDITION( a2->defaultValues().front() == SL( "val1" ) )
+	CHECK_CONDITION( a2->defaultValues().back() == SL( "val2" ) )
+}
+
 
 int main()
 {
