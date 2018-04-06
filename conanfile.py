ff130891b52a07dcfbe859f758d41614fcf3c1a1
Igor Mironchik <igor.mironchik@gmail.com>
2018-04-06 10:11:34 +0300

    Conan file.

--- conanfile.py
+++ conanfile.py
@@ -4,10 +4,14 @@ from conans import ConanFile
 class ArgsConan(ConanFile):
     name = "Args"
     version = "5.0.1.4"
-    url = "https://github.com/igormironchik/args-parser"
+    url = "https://github.com/igormironchik/args-parser.git"
     license = "MIT"
     description = "Small C++ header-only library for parsing command line arguments."
+    exports = "Args/*.hpp", "COPYING"
 
     def package(self):
         self.copy("COPYING", src=".", dst=".")
         self.copy("*.hpp", src="./Args", dst="./Args")
+        
+    def package_info(self):
+        self.cpp_info.includedirs = ["Args"]
