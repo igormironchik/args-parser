d7df11288864970bae959eb13f81d15cfd790e1f
Igor Mironchik <igor.mironchik@gmail.com>
2020-11-02 17:18:04 +0300

    Update version in Conan package.

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile
 
 class ArgsConan(ConanFile):
     name = "Args"
-    version = "5.0.1.6"
+    version = "6.0.0.0"
     url = "https://github.com/igormironchik/args-parser.git"
     license = "MIT"
     description = "Small C++ header-only library for parsing command line arguments."
