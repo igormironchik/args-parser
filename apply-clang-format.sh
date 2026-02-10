62026e0cc1114a3c14826ca66bbb2a25389285cf
Igor Mironchik <igor.mironchik@gmail.com>
2026-02-10 09:07:01 +0300

    Add script to apply clang format.

---
+++ apply-clang-format.sh
@@ -0,0 +1,5 @@
+clang-format --style=file -i --sort-includes args-parser/*.hpp
+clang-format --style=file -i --sort-includes samples/*/*.cpp
+clang-format --style=file -i --sort-includes tests/auto/*/*.cpp
+clang-format --style=file -i --sort-includes tests/auto/*/*.hpp
+clang-format --style=file -i --sort-includes tests/benchmark/main.cpp
